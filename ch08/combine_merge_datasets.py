f43de102e63175ed45bd76f8f70b11e3e17ee5fb
Yefan Zhang <yzhang@bluehole.net>
2019-12-20 17:22:57 +0100

    page 247

--- ch08/combine_merge_datasets.py
+++ ch08/combine_merge_datasets.py
@@ -88,3 +88,103 @@ righth
 
 # %%
 pd.merge(lefth, righth, left_on=['key1', 'key2'], right_index=True)
+
+# %%
+left2 = pd.DataFrame([[1., 2.], [3., 4.], [5., 6.]],
+                     index=['a', 'c', 'e'],
+                     columns=['Ohio', 'Nevada'])
+left2                     
+
+# %%
+right2 = pd.DataFrame([[7., 8.], [9., 10.], [11., 12.], [13, 14]],
+                      index=['b', 'c', 'd', 'e'],
+                      columns=['Missouri', 'Alabama'])
+right2                      
+
+# %%
+pd.merge(left2, right2, how='outer', left_index=True, right_index=True)
+
+# %%
+left2.join(right2, how='outer')
+
+# %%
+another = pd.DataFrame([[7., 8.], [9., 10.], [11., 12.], [16., 17.]],
+                       index=['a', 'c', 'e', 'f'],
+                       columns=['New York', 'Oregon'])
+another                       
+
+# %%
+left2.join([right2, another])
+
+# %%
+left2.join([right2, another], how='outer')
+
+# %%
+arr = np.arange(12).reshape((3, 4))
+arr
+
+# %%
+np.concatenate([arr, arr], axis=1)
+
+# %%
+s1 = pd.Series([0, 1], index=['a', 'b'])
+s2 = pd.Series([2, 3, 4], index=['c', 'd', 'e'])
+s3 = pd.Series([5, 6], index=['f', 'g'])
+
+pd.concat([s1, s2, s3])
+
+# %%
+pd.concat([s1, s2, s3], axis=1)
+
+# %%
+s4 = pd.concat([s1, s3])
+s4
+
+# %%
+pd.concat([s1, s4], axis=1)
+
+# %%
+pd.concat([s1, s4], axis=1, join='inner')
+
+# %%
+pd.concat([s1, s4], axis=1, join_axes=[['a', 'c', 'b', 'e']])
+
+# %%
+result = pd.concat([s1, s2, s3], keys=['one', 'two', 'three'])
+result
+
+# %%
+result.unstack()
+
+# %%
+df1 = pd.DataFrame(np.arange(6).reshape(3, 2), index=['a', 'b', 'c'],
+                   columns=['one', 'two'])
+df1                   
+
+# %%
+df2 = pd.DataFrame(5 + np.arange(4).reshape(2, 2), index=['a', 'c'],
+                   columns=['three', 'four'])
+df2                   
+
+# %%
+pd.concat([df1, df2], axis=1, keys=['level1', 'level2'])
+
+# %%
+pd.concat({'level1': df1, 'level2': df2}, axis=1)
+
+# %%
+a = pd.Series([np.nan, 2.5, np.nan, 3.5, 4.5, np.nan],
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+a              
+
+# %%
+b = pd.Series(np.arange(len(a), dtype=np.float64),
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+b[-1] = np.nan
+b
+
+# %%
+np.where(pd.isnull(a), b, a)
+
+# %%
+b[:-2].combine_first(a[2:])
---
+++ ch08/reshaping_pivoting.py
@@ -0,0 +1,61 @@
+#%%
+import pandas as pd
+import numpy as np
+
+data = pd.DataFrame(np.arange(6).reshape((2, 3)),
+                    index=pd.Index(['Ohio', 'Colorado'], name='state'),
+                    columns=pd.Index(['one', 'two', 'three'],
+                    name='number'))
+data                    
+
+# %%
+result = data.stack()
+result
+
+# %%
+result.unstack()
+
+# %%
+result.unstack(0)
+
+# %%
+result.unstack('state')
+
+# %%
+s1 = pd.Series([0, 1, 2, 3], index=['a', 'b', 'c', 'd'])
+s2 = pd.Series([4, 5, 6], index=['c', 'd', 'e'])
+data2 = pd.concat([s1, s2], keys=['one', 'two'])
+data2
+
+# %%
+data2.unstack()
+
+# %%
+data2.unstack(1)
+
+# %%
+df = pd.DataFrame({'left': result, 'right': result + 5},
+                  columns=pd.Index(['left', 'right'], name='side'))
+df                  
+
+# %%
+df.unstack('state')
+
+# %%
+df.unstack('state').stack('side')
+
+# %%
+data = pd.read_csv('examples/macrodata.csv')
+data.head()
+
+# %%
+periods = pd.PeriodIndex(year = data.year, quarter=data.quarter, name='date')
+columns = pd.Index(['realgdp', 'infl', 'unemp'], name='item')
+data = data.reindex(columns=columns)
+data.index = periods.to_timestamp('D', 'end')
+ldata = data.stack().reset_index().rename(columns={0: 'value'})
+
+# %%
+ldata[:10]
+
+# %%
